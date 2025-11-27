c5f58f616a192e4f4aeb8d2f114058035cb1a552
Andrew <menlod@yandex.ru>
2025-11-27 05:15:19 +0300

    lightningcss added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-// https://vite.dev/config/
+import browserslist from "browserslist";
+import { browserslistToTargets } from "lightningcss";
+
+const browserslistQuery = [
+  ">= 0.25%",
+  "last 2 versions",
+  "not dead",
+  "not op_mini all",
+];
+
+const targets = browserslistToTargets(
+  browserslist(browserslistQuery, { env: "production" })
+);
+
 export default defineConfig({
   plugins: [
     tanstackRouter({
@@ -10,4 +23,13 @@ export default defineConfig({
     }),
     react(),
   ],
+  css: {
+    transformer: "lightningcss",
+    lightningcss: {
+      targets,
+    },
+  },
+  build: {
+    cssMinify: "lightningcss",
+  },
 });
