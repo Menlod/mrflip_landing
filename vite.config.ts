d3a2f70fabcc420dce871aa4ce22232506ec292d
Andrew <menlod@yandex.ru>
2025-11-28 07:14:20 +0300

    fix repo name

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ const targets = browserslistToTargets(
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "/mrflip/" : "/",
+  base: process.env.GITHUB_PAGES ? "/mrflip_landing/" : "/",
   plugins: [
     tanstackRouter({
       target: "react",
